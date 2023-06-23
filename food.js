39602f67a4d4a0bead3ae53c95542e2884c66f26
amber-king <amberking@pursuit.org>
2023-06-23 15:20:27 -0400

    added prepare & isFresh method to Food class

--- food.js
+++ food.js
@@ -6,10 +6,20 @@ class Food {
     this.fresh = fresh;
   }
   prepare() {
-    console.log(`${this.name} is being prepared`)
+    console.log(`${this.name} is being prepared`);
   }
-  isFresh(){}
-  aDayPasses(){}
+  isFresh() {
+    if (this.daysToSpoil > 0) {
+      console.log(
+        `There are ${this.daysToSpoil} days left before ${this.name} spoils`
+      );
+    } else {
+      console.log(`${this.name} has spoiled.`);
+    }
+  }
+  aDayPasses() {
+    this.isFresh();
+  }
 }
 // Do not edit below this line
 module.exports = Food;
