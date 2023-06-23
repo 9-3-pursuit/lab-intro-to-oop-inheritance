f611206a4b6d1f1fadf709d1ca4ecb0a280a6315
amber-king <amberking@pursuit.org>
2023-06-23 15:13:06 -0400

    added empty class w/attributes for food.js

--- food.js
+++ food.js
@@ -1,4 +1,15 @@
 // Create class below
-
+class Food {
+  constructor(name, daysToSpoil, fresh = true) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  }
+  prepare() {
+    console.log(`${this.name} is being prepared`)
+  }
+  isFresh(){}
+  aDayPasses(){}
+}
 // Do not edit below this line
 module.exports = Food;
