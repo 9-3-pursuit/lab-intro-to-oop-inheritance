6798e3f929a9e604ef25d226c509a4ed67dcb7e9
amber-king <amberking@pursuit.org>
2023-06-23 18:08:22 -0400

    added in medicate & play methods to tamagotchi.js

--- tamagotchi.js
+++ tamagotchi.js
@@ -30,15 +30,52 @@ class Tamagotchi {
     this.energy -= 1;
     if (this.full > 10) this.sick = true;
   }
-  medicate() {}
-  play() {}
+  medicate() {
+    if (this.sick) {
+      this.full = 9;
+      this.energy -= 3;
+      this.sick = false;
+    } else {
+      console.log(`${this.name} needs no medicine because they are well.`);
+      this.energy -= 1;
+    }
+  }
+  play() {
+    if (this.sick) {
+      this.mood -= 1;
+      this.energy -= 1;
+      console.log(`Not feeling well, can't play`);
+    } else {
+      if (this.mood > 9) {
+        this, (energy -= 2);
+        this.full -= 1;
+      }
+      if (this.energy < 3) {
+        console.log(`I am too tired to play`);
+      }
+    }
+  }
   sleep() {}
   timePasses() {}
   badGuardian() {}
 }
 
-let cutie = new Tamagotchi("cutie", 3, 6, 2, false);
+let cutie = new Tamagotchi("cutie", 3, 6, 2, true, false);
 
-cutie.status();
+cutie.greet() // Hello, I'm cutie !
+
+cutie.status() // --v
+ // My mood is: 2;
+ // I am this full: 6;
+ // My energy is: 3;
+ // I am not sick
+
+// cutie.eat() // based off energy, sick & full levels
+ cutie.medicate()
+cutie.play() // I am too tired to play
+// cutie.sleep()
+// cutie.timePasses()
+// cutie.badGuardian()
+
 // Do not edit below this line
 module.exports = Tamagotchi;
