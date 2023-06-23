b5f6b0e3c9ec6aef3c512b16d1a4459260c00781
amber-king <amberking@pursuit.org>
2023-06-23 16:10:38 -0400

    added tamagotchi class set up w/attributes & greet method added on

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,6 +1,28 @@
 // Create class below
-class Tamagotchi extends Food{
-    
+class Tamagotchi {
+  constructor(
+    name,
+    energy = 9,
+    full = 8,
+    mood = 6,
+    sick = false,
+    rehomed = false
+  ) {
+    this.name = name;
+    this.energy = energy;
+    this.full = full;
+    this.mood = mood;
+    this.sick = sick;
+    this.rehomed = rehomed;
+  }
+  greet() {}
+  status() {}
+  eat() {}
+  medicate() {}
+  play() {}
+  sleep() {}
+  timePasses() {}
+  badGuardian() {}
 }
 // Do not edit below this line
 module.exports = Tamagotchi;
