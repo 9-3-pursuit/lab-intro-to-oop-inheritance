fcf7cc08bf0a7b15b96642bb343092ea84369ede
amber-king <amberking@pursuit.org>
2023-06-23 15:56:02 -0400

    aDayPasses method added, instantiate w/ details added & tested on food.js

--- food.js
+++ food.js
@@ -18,8 +18,22 @@ class Food {
     }
   }
   aDayPasses() {
+    this.daysToSpoil > 0 ? (this.daysToSpoil -= 1) : (this.daysToSpoil = 0);
     this.isFresh();
   }
 }
+
+// added the features/details of your methods here when adding a instantiate
+let sushi = new Food("sushi", 6, false)
+let yogurt = new Food("yogurt", 15, true)
+
+sushi.prepare() // sushi is being prepared
+sushi.isFresh() // There are 6 days left before sushi spoils
+sushi.aDayPasses() // There are 5 days left before sushi spoils
+
+yogurt.prepare() // yogurt is being prepared
+yogurt.isFresh() // There are 15 days left before yogurt spoils
+yogurt.aDayPasses() // There are 14 days left before yogurt spoils
+
 // Do not edit below this line
 module.exports = Food;
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,6 @@
 // Create class below
-
+class Tamagotchi extends Food{
+    
+}
 // Do not edit below this line
 module.exports = Tamagotchi;
