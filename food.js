5834241f8cc6c2d238c40cc6f00d8d4892432ae0
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-06-24 23:17:11 -0400

    create class food add attributes

--- food.js
+++ food.js
@@ -1,4 +1,23 @@
 // Create class below
 
+class Food {
+  constructor(name, daysToSpoil, fresh = true) {
+    this.name = name;
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+  }
+  prepare() {
+    console.log(this.walkStyle);
+  }
+  greet(otherBeing) {
+    console.log(`Sniff sniff, ${otherBeing}`);
+  }
+  classyGreeting(otherClassyBeing) {
+    console.log(`Howdy, there, ${otherClassyBeing.name}`);
+  }
+  ageUp() {
+    this.age++;
+  }
+}
 // Do not edit below this line
 module.exports = Food;
