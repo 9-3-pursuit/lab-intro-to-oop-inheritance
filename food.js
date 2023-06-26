50741703eed69cf7a4e016ca2b5beff130ce25c7
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-06-25 21:31:53 -0400

    completed food.js tests and started tomagotchi

--- food.js
+++ food.js
@@ -1,4 +1,56 @@
 // Create class below
+class Food {
+    constructor(name, daysToSpoil, fresh = true){
+    this.name = name; 
+    this.daysToSpoil = daysToSpoil;
+    this.fresh = fresh;
+    }
+    prepare(){
+    console.log(`${this.name} is being prepared`)
+    } 
+    isFresh(){ 
+        if(this.daysToSpoil <= 0){
+            this.fresh = false
+        } else{
+            this.fresh = true
+        }
 
+        if(this.fresh === true){
+        console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`)
+    }
+      else  {
+        console.log(`${this.name} has spoiled.`)
+    }
+    }
+
+
+aDayPasses(){
+    if(this.daysToSpoil > 0 ){
+        // this.daysToSpoil = this.daysToSpoil -1
+        // this.daysToSpoil -= 1
+        this.daysToSpoil -- 
+    }
+   this.isFresh()
+    // if(this.daysToSpoil === 0) {
+    //     this.fresh = false
+    // }
+}
+
+
+
+
+
+
+}
 // Do not edit below this line
 module.exports = Food;
+
+
+let x = new Food(`tacos`, 2)
+
+
+x.prepare()
+x.isFresh() 
+x.aDayPasses()
+x.aDayPasses()
+
--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,13 @@
 // Create class below
-
+class Tamagotchi{
+    constructor(name, energy, full, mood, sick = false, rehomed = false){
+this.name = name; 
+this.energy = energy;
+this.full = full;
+this.mood = mood;
+this.sick = sick; 
+this.rehomed = rehomed;
+}
+}
 // Do not edit below this line
 module.exports = Tamagotchi;
