e7f6b695b9a2e53ed9589eda74b2343702f027cf
touhami-ben <brooklyn.ben76@gmail.com>
2023-06-26 00:50:03 -0400

    add medicate method to tamagotchi class

--- tamagotchi.js
+++ tamagotchi.js
@@ -12,6 +12,38 @@ class Tamagotchi{
     greet(){
        console.log(`Hello, I'm ${this.name}`);
     }
+
+    status(){
+        console.log(`My mood is: ${this.mood} \n I am this full: ${this.full}\n My energy is: ${this.energy} \n I am ${this.sick ? "" : "not "}sick\n\n`)
+    }
+    eat(){
+        this.full += 2;
+        this.energy--;
+        if(this.full > 10){
+            this.sick = true;
+        }
+    }
+
+    medicate(){
+        if (this.sick) {
+            this.full = 9;
+            this.energy -= 3;
+            this.sick = false;
+          } else {
+            console.log(`refusal to take medicine`);
+            this.energy--;
+          }
+    }
+
 }
+
+const pet = new Tamagotchi("Albert", 2, 3, 5, false, true);
+ pet.status();
+ while (!pet.sick){
+    pet.eat();
+    pet.status();
+ }
+
+
 // Do not edit below this line
 module.exports = Tamagotchi;
