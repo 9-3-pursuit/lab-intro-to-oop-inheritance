79ac82a0bb2bd4fccf6972ab938caef1713175c1
ariunaamy <ariun.myagmar@gmail.com>
2023-06-26 19:50:27 -0400

    all tests passed

--- tamagotchi.js
+++ tamagotchi.js
@@ -16,11 +16,68 @@ class Tamagotchi{
             `My mood is: ${this.mood}\nI am this full: ${this.full}\nMy energy is: ${this.energy}\nI am ${!this.sick ? "not sick" : "sick"}`
         )
     }
-    
+    eat(){
+        this.full+=2
+        this.energy-=1;
+        if(this.full>10){
+            this.sick=true;
+        }
+    }
+    medicate(){
+        if(this.sick){
+            this.full=9;
+            this.energy-=3;
+            this.sick=false;
+        }else{
+            console.log("no, I don't want medicine!")
+            this.energy-=1;
+        }
+    }
+    play(){
+        if(this.sick){
+            this.mood-=1;
+            this.energy-=1;
+        }else if(this.mood>9){
+            this.energy-=2;
+            this.full-=1;
+        }else if(this.energy<=3){
+            console.log ("I am too tired to play");
+            this.energy-=1;
+        }
+        else{
+            this.mood+=2;
+            this.energy-=1;
+            this.full-=1;
+        }   
+    }
+    sleep(){
+        this.energy+=4;
+        this.full-=3;
+    }
+    timePasses(){
+        if(!this.sick){
+            this.mood-=2;
+            this.full-=1;
+            this.energy-=1;
+        }else{
+            this.mood-=3;
+            this.full-=2;
+            this.energy-=2;
+        }
+    }
+    badGuardian(){
+        console.log("the tamagotchi has been rehomed");
+        if(this.energy<=0 || this.full<=0){
+            this.rehomed=true; 
+        }
+
+    }
+    }
 
 
 
-}
+
+
 
 let baby = new Tamagotchi("Baby")
 console.log(baby.greet())
