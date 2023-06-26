7f70ce863f51372b92bc4cc4f34e659eb507bc3e
touhami-ben <brooklyn.ben76@gmail.com>
2023-06-26 00:14:44 -0400

    add properties to tamagotchi class

--- food.js
+++ food.js
@@ -11,11 +11,23 @@
 
       isFresh(){
         if(this.daysToSpoil > 0){
-            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils`);
+            console.log(`There are ${this.daysToSpoil} days left before ${this.name} spoils.`);
         }else {
             console.log(`${this.name} has spoiled.`)
         }
       }
+
+      aDayPasses(){
+        this.daysToSpoil--;
+        this.isFresh();
+      }
+ }
+ //testing 
+ const salmon = new Food("salmon", 4);
+ salmon.prepare();
+ salmon.isFresh();
+ for (let i = 0 ; i < 6; i++){
+    salmon.aDayPasses();
  }
 // Do not edit below this line
 module.exports = Food;
