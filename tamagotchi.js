234102cbc8b15aa959fd8f0b7824b7f43bb0603b
touhami-ben <brooklyn.ben76@gmail.com>
2023-06-26 01:01:28 -0400

    add sleep method and timepasses method to tamagotchi class

--- tamagotchi.js
+++ tamagotchi.js
@@ -50,6 +50,21 @@ class Tamagotchi{
           this.full--;
         }
       }
+      sleep() {
+        this.energy += 4;
+        this.full -= 3;
+      }
+      timePasses() {
+        if (!this.sick) {
+          this.mood -= 2;
+          this.full--;
+          this.energy--;
+        } else {
+          this.mood -= 3;
+          this.full -= 2;
+          this.energy -= 2;
+        }
+      }
 
 }
 
