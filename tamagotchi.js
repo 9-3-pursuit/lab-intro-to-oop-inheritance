5cf91ae84864d0195960b52da64f77379ca128e0
Karimah Reavis <Karimah586@gmail.com>
2023-06-27 23:33:49 -0400

    halfway done

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,22 @@
 // Create class below
-
+class Tamagotchi{
+    constructor(name,energy=9,full=9,mood=6,sick=false,rehomed=false){
+        this.name=name;
+        this.energy=energy;
+        this.full=full;
+        this.mood=mood;
+        this.sick=sick;
+        this.rehomed=rehomed;
+    }
+    greet(){
+        console.log(`Hello, I'm ${this.name}!`)
+    }
+    status(){
+        console.log(`My mood is: ${this.mood} \nI am this full: ${this.full} \nMy energy is: ${this.energy} \nI am not sick`)
+    }
+    eat(){
+        
+    }
+}
 // Do not edit below this line
 module.exports = Tamagotchi;
