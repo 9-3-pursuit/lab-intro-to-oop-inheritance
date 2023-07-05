194542836cbdbe2d74dd1bfc8ec322c8be34b523
Markeya Mckoy-Carree <mcmcarree@gmail.com>
2023-07-05 10:54:03 -0400

    added methods

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,23 @@
 // Create class below
+class Tamagotchi{
+    constructor(name, energy, full, mood, sick, rehomed){
+        this.name = name;
+        this.energy = energy || 9;
+        this.full = full || 8;
+        this.mood = mood || 6;
+        this.sick = sick || false;
+        this.rehomed = rehomed || false 
+    }
+    greet(){
+        console.log(`Hello, I'm ${this.name}`)
+    }
+}
+
+status(){
+    console.log(`My mood is ${this.mood}, I am this full ${this.full}, my energy is ${this.energy}, I am ${this.sick}`)
+}
+
+
 
 // Do not edit below this line
 module.exports = Tamagotchi;
