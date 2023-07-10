8740bf7f25de8dc9623cb4c7cca5405f3e727ec0
elizabethbright721 <elizabethbright721@gmail.com>
2023-07-10 08:51:52 -0400

    updated food.js with attributes and methods

--- food.js
+++ food.js
@@ -1,4 +1,25 @@
 // Create class below
-
+class Food {
+    constructor(name, daysToSpoil, isFresh=true){
+        this.name = name;
+        this.daysToSpoil = daysToSpoil;
+        this.isFresh = isFresh;
+    }
+    prepare(){
+        console.log(`${this.name} is being prepared`)
+    }
+    isFresh(){
+        if(this.daysToSpoil > 0){
+            console.log(`There are ${this.isFresh} days left before ${this.name} spoils.`)
+        }else {
+            console.log(`${this.name} has spoiled.`)
+        }
+    }
+    aDayPasses(){
+        this.daysToSpoil--;
+        this.isFresh();
+    }
+    
+}
 // Do not edit below this line
 module.exports = Food;
