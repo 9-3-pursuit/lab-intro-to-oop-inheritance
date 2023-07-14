3223df2aef02e87f876dcd9c8b4169d5a5f28e1e
amber-king <amberking@pursuit.org>
2023-07-14 12:03:03 -0400

    edit console.log for .isFresh() method

--- food.js
+++ food.js
@@ -12,7 +12,7 @@ class Food {
   isFresh() {
     if (this.daysToSpoil > 0) {
       console.log(
-        `There are ${this.daysToSpoil} days left before ${this.name} spoils. `
+        `There are ${this.daysToSpoil} days left before ${this.name} spoils.`
       );
     } else {
       console.log(`${this.name} has spoiled.`);
